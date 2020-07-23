122a13f364ffb8782375e4cb6b1ee44e8805d5d0
jsgygujun <jsgygujun@gmail.com>
2020-07-23 09:48:55 +0800

    [ADD] Hello Scala

---
+++ example/src/main/scala/com/jsgygujun/chapter01/Hello.scala
@@ -0,0 +1,14 @@
+package com.jsgygujun.chapter01
+
+/**
+ *
+ * @author gujun@qiyi.com
+ * @since 2020/7/23 9:48 上午
+ */
+object Hello {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, scala!")
+  }
+
+}
