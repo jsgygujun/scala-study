1b088d23f98ca0edb5a3228b738cdb9429bfa023
jsgygujun <jsgygujun@gmail.com>
2020-07-24 10:25:29 +0800

    [UPDATE] ARRAY

--- example/src/main/scala/com/jsgygujun/chapter07/ArrayExp.scala
+++ example/src/main/scala/com/jsgygujun/chapter07/ArrayExp.scala
@@ -1,5 +1,7 @@
 package com.jsgygujun.chapter07
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
  *
  * @author jsgygujun@gmail.com
@@ -13,6 +15,8 @@ object ArrayExp {
     // (10)，表示数组的大小，确定后就不可以变化
     val arr1 = new Array[Int](10)
     println(arr1.length) // 10
+
+    val s = Array("Hello", "World") // 长度为2类型为String的数组
 
     // 数组赋值的两种方式
     arr1(3) = 10
@@ -28,6 +32,32 @@ object ArrayExp {
     def p(e: Int): Unit = print(e+",")
     arr1.foreach(p)
     arr1.foreach(println(_))
+
+
+    // 变长数组
+    val b = ArrayBuffer[Int]() // 或者 new ArrayBuffer[Int]，一个空的数组缓冲，准备存放整数
+    b += 1 // ArrayBuffer(1)，用 += 在尾端添加元素
+    b += (1,2,3,4,5) // ArrayBuffer(1,1,2,3,4,5), 在尾端追加多个元素
+    b ++= Array(8,13,21) // ArrayBuffer(1,1,2,3,4,5,8,13,21) ++= 操作符追加任何集合
+    b.trimEnd(5) // 移除最后5个元素
+    b.toArray // 转数组
+
+
+    // 数组转换，不修改原数组，而是产生一个全新的数组
+    val a = Array(2,3,5,7,11)
+    val result = for (e <- a) yield 2 * e // Array(4,6,10,14,22)
+    val aa = a.filter(_ % 2 == 0).map(_ * 2) // 过滤 + 转化
+
+    // 给定一个数组缓冲，移除除第一个负数意外的所有负数
+    // 1. 搜集需要保留的下标，2. 将元素移动到该去的地方，并切断尾端
+    var array = ArrayBuffer(3,-1,3,-4,3,-9,2,4)
+    var first = true
+    val index = for (i <- array.indices if first || array(i) >= 0) yield {
+      if (array(i) < 0) first = false; i
+    }
+    for (j <- index.indices) array(j) = a(index(j))
+    array.trimEnd(array.length-index.length)
+
 
   }
 }
