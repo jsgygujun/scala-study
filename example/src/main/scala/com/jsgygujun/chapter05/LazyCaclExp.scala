4e4fe4382fc42f8bdd8d1e377c05396b5e89f930
jsgygujun <jsgygujun@gmail.com>
2020-08-04 11:59:40 +0800

    [ADD] LazyCaclExp

---
+++ example/src/main/scala/com/jsgygujun/chapter05/LazyCaclExp.scala
@@ -0,0 +1,21 @@
+package com.jsgygujun.chapter05
+
+/**
+ * 惰性求值
+ *
+ * @author jsgygujun@gmail.com
+ * @since 2020/8/4 11:56 上午
+ */
+object LazyCaclExp {
+
+  def main(args: Array[String]): Unit = {
+    lazy val res = sum(10, 29)
+    println("--"*10)
+    println("res: " + res)
+  }
+
+  def sum(x: Int, y: Int) = {
+    println("sum 被执行")
+    x + y
+  }
+}
